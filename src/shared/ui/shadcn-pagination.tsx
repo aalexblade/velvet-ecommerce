602c1c5784ae6d684d936c44b66048a5e7b09d19
alex blade <aalexblade@gmail.com>
2026-07-31 22:56:28 +0300

    feat(shared-ui): add base shadcn pagination component

---
+++ src/shared/ui/shadcn-pagination.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { cn } from "@/shared/lib/utils";
+
+export const PaginationRoot = ({
+  className,
+  ...props
+}: React.ComponentProps<"nav">) => (
+  <nav
+    role="navigation"
+    aria-label="pagination"
+    className={cn("mx-auto flex w-full justify-center", className)}
+    {...props}
+  />
+);
+
+export const PaginationContent = React.forwardRef<
+  HTMLUListElement,
+  React.ComponentProps<"ul">
+>(({ className, ...props }, ref) => (
+  <ul
+    ref={ref}
+    className={cn("flex flex-row items-center gap-1", className)}
+    {...props}
+  />
+));
+PaginationContent.displayName = "PaginationContent";
+
+export const PaginationItem = React.forwardRef<
+  HTMLLIElement,
+  React.ComponentProps<"li">
+>(({ className, ...props }, ref) => (
+  <li ref={ref} className={cn("", className)} {...props} />
+));
+PaginationItem.displayName = "PaginationItem";
